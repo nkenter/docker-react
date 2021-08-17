e3f4ec5c862d9ee26f23da091ef6a7108c20c3b1
nkenter <nissimkenter@gmail.com>
2021-08-17 11:33:21 -0400

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+// let copy the test again to have 2 tests
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
